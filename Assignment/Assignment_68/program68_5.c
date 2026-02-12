f1ec64d53f52fa376ee0979c4c7f6abd0174e777
Vivek Gautam <vivekbgautam@gmail.com>
2026-02-12 11:34:21 +0530

    Add files via upload

---
+++ Assignment/Assignment_68/program68_5.c
@@ -0,0 +1,44 @@
+////////////////////////////////////////////////////////////
+//
+// Required header for program 
+//
+////////////////////////////////////////////////////////////
+#include<stdio.h>
+
+///////////////////////////////////////////////////////////
+//
+//  Function Name : Display()
+//  Description   : It is a recursive function 
+//                  Which Display the pattern
+// Input          : Number of patter to print 
+// Output         : Display Desired Pattern on Scren
+// Date           : 11-02-2026
+// Author         : Vivek bHauraj Gautam
+//
+////////////////////////////////////////////////////////////    
+void Display(char Ch)
+{
+    if(Ch <= 'f')
+    {
+        printf("%c\t",Ch);
+        Ch++;
+        Display(Ch);
+    }
+}
+
+////////////////////////////////////////////////////////////
+//
+// Function name : Main
+// Description   : It is a entry point of our program 
+//
+////////////////////////////////////////////////////////////
+int main()
+{
+    char Value = '\0';
+    printf("Enter the number : ");
+    scanf("%c",&Value);
+
+    Display(Value);
+
+    return 0;
+}
